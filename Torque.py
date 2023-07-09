e363453a1a3d562c0cb295cb550710092892b39c
KimDia <139007251+KimDia@users.noreply.github.com>
2023-07-09 15:20:00 +0900

    Add files via upload

--- Torque.py
+++ Torque.py
@@ -5,15 +5,15 @@ st.write('')
 st.write('#### 1. Pedestal information')
 length = st.slider('How long the pedestal is? (m)', 0, 100)
 rotation = st.slider('Where is the rotation axis? (Based on the far left, m)', 0, int(length))
-WeightPedestal = st.text_input('What is the mass of the pedestal? (kg)')
+MassPedestal = st.text_input('What is the mass of the pedestal? (kg)')
 st.write('')
 st.write('#### 2. object information')
-WeightObject = st.text_input('What is the mass of the object? (kg)')
+MassObject = st.text_input('What is the mass of the object? (kg)')
 object = st.slider('Where is the object? (Based on the far left, m)', 0, int(length))
 st.write('')
 if st.button('Calculate'):
     st.write('Done!')
-    result = -((int(length)/2)-int(rotation))*int(WeightPedestal)+(int(rotation)-int(object))*int(WeightObject)
+    result = -((int(length)/2)-int(rotation))*int(MassPedestal)+(int(rotation)-int(object))*int(MassObject)
     if result<0:
         st.write('### The direction of the torque is clockwise, and its magnitude is', abs(result)*10, 'N.')
     elif result>0:
