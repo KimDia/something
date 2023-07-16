374df3ec7cf7d1e0e9b1e238e2f5b71e0eb66000
KimDia <139007251+KimDia@users.noreply.github.com>
2023-07-16 21:21:20 +0900

    Add files via upload

--- Torque.py
+++ Torque.py
@@ -8,29 +8,45 @@ st.title('Torque calculator')
 st.write('#### This online calculator calculates the direction and magnitude of the torque.')
 st.write('')
 
+#받침대 정보 입력
 st.header('1. Beam information')
-length = int(st.slider('How long the Beam is? (m)', 0, 100))
-rotation = int(st.slider('Where is the rotation axis? (Based on the far left, m)', 0, length))
-MassBeam = float(st.text_input('What is the mass of the Beam? (kg)'))
+BeamLength = int(st.slider('How long the Beam is? (m)', 0, 100))
+RotationPosition = int(st.slider('Where is the rotation axis? (Based on the far left, m)', 0, BeamLength))
+BeamMass = float(st.text_input('What is the mass of the Beam? (kg)'))
 st.write('')
 
+#받침대가 만들어내는 돌림힘 구하기(반시계 방향이 음수)
+BeamTorque = ((BeamLength/2)-RotationPosition)*BeamMass*10
+
+#물체 갯수 입력
 st.header('2. Object information')
-MassObject = float(st.text_input('What is the mass of the object? (kg)'))
+count = int(st.text_input('How many objects are there?'))
 
-object = int(st.slider('Where is the object? (Based on the far left, m)', 0, length))
-st.write('')
+ObjectTorque = 0
+
+#물체 갯수(번)만큼 물체의 질량과 위치 입력받기
+for i in range (count):
+    st.write('')
+    st.write(f' #### {i+1}. Object {i+1}')
+    ObjectMass = float(st.text_input('What is the mass of the object? (kg)', key = f'{i+1}'))
+    ObjectPosition = int(st.slider('Where is the object? (Based on the far from left, m)', 0, BeamLength, key = f'{ObjectMass}_{i+1}'))
+
+    #물체가 만들어내는 알짜돌림힘
+    ObjectTorque += (ObjectPosition-RotationPosition)*ObjectMass*10
+
+#받침대와 물체가 만들어내는 알짜돌림힘
+TorqueSum = BeamTorque+ObjectTorque
 
 if st.button('Calculate'):
     st.write('Done!')
-    result = -((length/2)-rotation)*MassBeam+(rotation-object)*MassObject
-
-    if result<0:
-        st.write('### The direction of the torque is clockwise, and its magnitude is', abs(result)*10, 'N.')
+    
+    if TorqueSum>0:
+        st.write('### The direction of the torque is clockwise, and its magnitude is', abs(TorqueSum), 'N.')
         image1 = Image.open('TorqueClockwise.png')
         st.image(image1)
 
-    elif result>0:
-        st.write('### The direction of the torque is counterclockwise, and its magnitude is', abs(result)*10, 'N.')
+    elif TorqueSum<0:
+        st.write('### The direction of the torque is counterclockwise, and its magnitude is', abs(TorqueSum), 'N.')
         image2 = Image.open('TorqueCounterclockwise.png')
         st.image(image2)
 
